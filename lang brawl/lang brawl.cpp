74b9f89af5e0a00a7540799ed426b8f2a4e89a61
lambda-san7 <codekiwi132@gmail.com>
2023-10-13 10:44:38 -0700

    stupid fucking SFML can't even load a fucking BMP

--- lang brawl/lang brawl.cpp
+++ lang brawl/lang brawl.cpp
@@ -1,17 +1,34 @@
+/////////////////////////////
+// INCLUDES
+/////////////////////////////
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(900, 600), "Lang Brawl");
+int main(){
+    system("dir");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Image");
 
-    while (window.isOpen()) {
+    sf::Texture texture;
+
+    if (!texture.loadFromFile("assets/bmp.bmp")) {
+        std::cout << "fuck you bitch";
+    }
+
+    sf::Sprite sprite;
+
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0,0,64,64));
+
+    while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event)) { 
+        while (window.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
+        window.draw(sprite);
         window.display();
     }
 }
